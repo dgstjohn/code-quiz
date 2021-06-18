a438787526ba44bfd93192725c457f0d2a604757
dgstjohn <dgstjohn@gmail.com>
2021-06-17 23:27:03 -0400

    initial commit with basic file setups

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+var startBtn = document.querySelector("click", function {
+    
+})
+
+var questions = [
+    { q: 'Commonly used data types do NOT include:', 
+    [ '1. strings', '2. booleans', '3. alerts', '4. numbers' ]
+    a: '2' },
+    { q: 'The condition in an if/else statement is enclosed within _____:',
+    [ '1. quotes', '2. curly brackets', '3. parentheses', '4. square brackets']
+    a: '1' },
+    { q: 'Arrays in JavaScript can be used to store ______:',
+    [ '1. numbers and strings', '2. other arrays', '3. booleans', '4. all of the above']
+    a: '3' },
+    { q: 'String values must be enclosed within ______ when being assigned to variables.',
+    [ '1. commas', '2. quotes', '3. curly brackets', '4. parentheses']
+    a: '1' },
+    { q: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+    [ '1. JavaScript', '2. terminal/bash', '3. for loops', '4. console.log']
+    a: '3' }
+  ];
