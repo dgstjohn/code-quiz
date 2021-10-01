22c2a518ba064a6308878d54e09a715f9c40f7e3
dgstjohn <dgstjohn@gmail.com>
2021-10-01 00:01:09 -0400

    highscores file all set

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,23 +1,27 @@
-function showScores {
+function showScores () {
 
     // retrieve existing high scores or set empty array to catch new scores
     var highScores = JSON.parse(localStorage.getItem("highscores")) || [];
 
+    // sort with highest score at top
+    highScores.sort(function(a,b) {
+        return b.score - a.score;
+    });
+
     // loop through retrieved scores and display
-    highScores.forEach() {
+    highScores.forEach(function(score) {
         var showScore = document.createElement("li");
         showScore.textContent = score.initials + ": " + score.score;
         var getScores = document.getElementById("highScoreList");
         getScores.appendChild(showScore);
-    }
+    })
 };
 
 // clear high scores 
 var clearHighScores = document.querySelector("#clearHighScoreBtn");
-
 clearHighScores.addEventListener("click", function () {
     localStorage.removeItem("highscores");
 });
 
-// call the function
+// call the showScores function
 showScores();
